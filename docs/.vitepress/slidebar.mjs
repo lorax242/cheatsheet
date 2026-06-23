43593b89bc30097976adc4a1f418a569913f1a9a
Ondrej.F <filipcik-117@seznam.cz>
2026-06-23 20:25:03 +0200

    oprava v nazvu fotokoutku

--- docs/.vitepress/slidebar.mjs
+++ docs/.vitepress/slidebar.mjs
@@ -131,7 +131,7 @@ export const sidebar = [
     
           section("Programy", [
             section("Fotokoutek", [
-            page("Instalace a konfigurace", "/Linux/Fotobudka/Instrukce_programu"),
+            page("Instalace a konfigurace", "/Linux/Fotobudka/Instalace_programu"),
           ]),
           ]),
 
